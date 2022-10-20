f2d5f7b33081d3e044e6b755c7ae18552ac8bd9c
Cem GEÇGEL <gecgelcem@outlook.com>
2022-10-20 23:14:24 +0300

    add: Test Time Measurement

--- src/test.c
+++ src/test.c
@@ -7,6 +7,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* A function that returns the result of a unit test. */
 typedef bool (*UnitTest)(void);
@@ -40,9 +41,18 @@ size_t RegisteredUnitTests = 0;
 
 /* Register the given unit test information. */
 void RegisterUnitTestInfo(UnitTestInfo const registered) {
+  // Check existance.
+  for (size_t test = 0; test < RegisteredUnitTests; test++) {
+    if (registered.callable == UnitTestArray[test].callable) {
+      fprintf(stderr, "Test `%s` is already registered!\n", registered.name);
+      abort();
+      return;
+    }
+  }
+
   // Check the capacity.
   if (RegisteredUnitTests == UNIT_TEST_CAPACITY) {
-    fputs("Out of unit test capacity!\n", stderr);
+    fprintf(stderr, "No space to add test `%s`!\n", registered.name);
     abort();
     return;
   }
@@ -51,25 +61,53 @@ void RegisterUnitTestInfo(UnitTestInfo const registered) {
   UnitTestArray[RegisteredUnitTests++] = registered;
 }
 
+/* Current UTC time in seconds. */
+long double GetTimeInSeconds() {
+  // Get the time and join the seconds with the nanoseconds.
+  struct timespec now;
+  timespec_get(&now, TIME_UTC);
+  long double const nanoScale = 1e9;
+  return (long double)now.tv_sec + now.tv_nsec / nanoScale;
+}
+
 /* Run all the unit tests. */
 void RunUnitTests(void) {
+  // Store the time for measuring the total time taken.
+  long double const start = GetTimeInSeconds();
+
   // Run all the tests and count the failed ones.
   size_t failedTests = 0;
   for (size_t test = 0; test < RegisteredUnitTests; test++) {
+    // Store the time for measuring the time taken by currently tested unit.
+    long double const unitStart = GetTimeInSeconds();
+
+    // Test and report.
     UnitTestInfo const tested = UnitTestArray[test];
     if (tested.callable()) {
-      printf("[PASS] %s\n", tested.name);
+      printf("[PASS]");
     } else {
-      fprintf(stderr, "[FAIL] %s\n", tested.name);
+      printf("[FAIL]");
       failedTests++;
     }
+
+    // Find the elapsed time for the currently tested unit.
+    long double const milliScale          = 1e3;
+    long double const elapsedSeconds      = GetTimeInSeconds() - unitStart;
+    long double const elapsedMilliseconds = elapsedSeconds * milliScale;
+    printf(" %s (%.3Lf ms)\n", tested.name, elapsedMilliseconds);
   }
 
   // Print whether there were failed tests.
-  if (RegisteredUnitTests == 0) puts("\nThere are no registered tests!\n");
-  else if (failedTests == 0) puts("\nAll tests passed.\n");
-  else if (failedTests == 1) fputs("\nA test failed!\n", stderr);
-  else fprintf(stderr, "\n%zu tests failed!\n", failedTests);
+  if (RegisteredUnitTests == 0) printf("\nThere are no registered tests!");
+  else if (failedTests == 0) printf("\nAll tests passed.");
+  else if (failedTests == 1) printf("\nA test failed!");
+  else printf("\n%zu tests failed!", failedTests);
+
+  // Find the total elapsed time.
+  long double const milliScale          = 1e3;
+  long double const elapsedSeconds      = GetTimeInSeconds() - start;
+  long double const elapsedMilliseconds = elapsedSeconds * milliScale;
+  printf(" (%.3Lf ms)\n", elapsedMilliseconds);
 }
 
 /* Always passes to check the test infrastructure. */
