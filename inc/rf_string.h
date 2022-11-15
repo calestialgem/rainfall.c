66f2a1b9c39151ecac4727d742ccc0b533f78bcb
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-15 23:10:40 +0300

    add: String

---
+++ inc/rf_string.h
@@ -0,0 +1,28 @@
+/* Represents strings and provides functions for manipulating them. */
+#ifndef RF_STRING_H
+#define RF_STRING_H 1
+
+#include <stddef.h>
+
+/* Immutable view of linearly stored group of characters. The string is NOT
+ * guaranteed to be null-terminated. */
+struct rf_string {
+  /* Immutable pointer to the first character in the string if it exists. Can be
+   * an invalid pointer if the count is 0. */
+  char const* data;
+  /* Number of characters in the string. */
+  size_t      count;
+};
+
+// =================================================
+//    }-{   P U B L I C   F U N C T I O N S   }-{
+// =================================================
+
+// =====================================================
+//    ***   Q U A L I T Y   A S S E S S M E N T   ***
+// =====================================================
+
+/* Run all the tests for the string module. */
+void rf_string_test(void);
+
+#endif // RF_STRING_H
