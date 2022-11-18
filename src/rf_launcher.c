9b9474098034fd232e47df8eb44408569421b9d1
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-18 14:20:34 +0300

    add: Unimplemented Message

--- src/rf_launcher.c
+++ src/rf_launcher.c
@@ -1,7 +1,19 @@
 #include "rf_launcher.h"
 
+#include <stdio.h>
+
 // =================================================
 //    }-{   P U B L I C   F U N C T I O N S   }-{
 // =================================================
 
-void rf_launch(struct rf_launch_command launched) {}
+void rf_launch(struct rf_launch_command launched) {
+  switch (launched.variant) {
+  case RF_LAUNCH_COMMAND_NEW:
+  case RF_LAUNCH_COMMAND_CHECK:
+  case RF_LAUNCH_COMMAND_TEST:
+  case RF_LAUNCH_COMMAND_BUILD:
+  case RF_LAUNCH_COMMAND_RUN:
+    fputs("failure: Not implemented yet!", stderr);
+    break;
+  }
+}
