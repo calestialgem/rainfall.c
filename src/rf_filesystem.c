fe857ae893e7ffdbad44b9412318b6c519bf08bb
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-20 11:56:08 +0300

    add: Opening and Closing Files

--- src/rf_filesystem.c
+++ src/rf_filesystem.c
@@ -1,5 +1,14 @@
 #include "rf_filesystem.h"
 
+#include "rf_string.h"
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 // Compile the implementation for the correct platform.
 #ifdef _WIN32
   #include "rf_filesystem_windows.c"
@@ -8,3 +17,66 @@
   // cannot be compiled.
   #error Filesystem only supports the fallowing platforms: Windows.
 #endif
+
+/* Writes the given string to the given buffer at the given index. Moves the
+ * index to the position just after the written string. */
+#define WRITE_STRING(buffer, index, string)             \
+  do {                                                  \
+    memcpy(buffer + index, string.array, string.count); \
+    index += string.count;                              \
+  } while (false)
+
+// =================================================
+//    }-{   P U B L I C   F U N C T I O N S   }-{
+// =================================================
+
+bool rf_open_file(struct rf_file* target, char const* mode,
+  char const* extension, int part_count, ...) {
+  // Convert variable length part array to a real array.
+  va_list variable_length_string_array;
+  va_start(variable_length_string_array, part_count);
+  struct rf_string parts[part_count];
+  for (int i = 0; i < part_count; i++) {
+    parts[i] = va_arg(variable_length_string_array, struct rf_string);
+  }
+  va_end(variable_length_string_array);
+
+  // Get the total length of parts, the extension and the directory separators.
+  struct rf_string extension_as_string = rf_view_null_terminated(extension);
+  size_t total_length = extension_as_string.count + max(0, part_count - 1);
+  for (int i = 0; i < part_count; i++) { total_length += parts[i].count; }
+
+  // Allocate an extra byte for the null-termination character.
+  target->path = malloc(total_length + 1);
+
+  // Write the parts, the directory separators, the extension and the
+  // null-termination character.
+  size_t current_write_index = 0;
+  WRITE_STRING(target->path, current_write_index, parts[0]);
+  for (int i = 1; i < part_count; i++) {
+    target->path[current_write_index++] = '/';
+    WRITE_STRING(target->path, current_write_index, parts[i]);
+  }
+  WRITE_STRING(target->path, current_write_index, extension_as_string);
+  target->path[current_write_index] = 0;
+
+  // Get the error code; as `fopen` does not guarantee to set `errno`, `fopen_s`
+  // is used because it guarantees to return an error code, which could be
+  // manually assigned to `errno`.
+  int error_code = fopen_s(&target->stream, target->path, mode);
+  if (error_code != 0) {
+    errno = error_code;
+    return true;
+  }
+  return false;
+}
+
+bool rf_close_file(struct rf_file* target) {
+  free(target->path);
+  target->path = NULL;
+
+  // Close the file and return the error status.
+  int status     = fclose(target->stream);
+  target->stream = NULL;
+  return status == EOF;
+}
--- src/rf_filesystem.h
+++ src/rf_filesystem.h
@@ -6,6 +6,20 @@
 #include "rf_string.h"
 
 #include <stdbool.h>
+#include <stdio.h>
+
+/* Combination of path and stream to a file. This is necessary, because the file
+ * paths must be null-terminated. `rf_string`s are not null-terminated and they
+ * might be required to be joined together to create a path. Thus, the path is
+ * dynamicly allocated and it is managed together with the path. */
+struct rf_file {
+  /* Owned, null-terminated character array as the path to the file. The path
+   * might be relative or absolute. */
+  char* path;
+  /* Owned stream to and from the file. The stream might be opened with any
+   * mode. */
+  FILE* stream;
+};
 
 // =================================================
 //    }-{   P U B L I C   F U N C T I O N S   }-{
@@ -17,5 +31,18 @@ bool rf_change_working_directory(struct rf_string path);
 /* Creates a new directory at the given path. Returns whether the
  * operation was not successful. Populates the `errno` on failure. */
 bool rf_create_directory(struct rf_string path);
+/* Opens the file at the path that is created by joining the given part strings,
+ * whose count must be given, with directory separators in between and the given
+ * extension juxtaposed at the end. Hence, the final path to the opened file
+ * would be `<part1>/<part2>/.../<partN><extension>`. Writes its information to
+ * the given target. Atleast a single part must be given and the number of
+ * `rf_string`s must be equal to the given count. Returns whether the operation
+ * was not successful. Populates the `errno` on failure. */
+bool rf_open_file(struct rf_file* target, char const* mode,
+  char const* extension, int part_count, ...);
+/* Closes the given file and sets its pointers to `NULL`. Might fail; for
+ * example, because closing a file flushes the buffered output. Returns whether
+ * the operation was not successful. Populates the `errno` on failure. */
+bool rf_close_file(struct rf_file* target);
 
 #endif // RF_FILESYSTEM_H
