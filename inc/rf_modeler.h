9a5227dc5fefa1ca8f2429da4a7442fd377f56e2
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-22 22:54:33 +0300

    add: Workspace Modeler

---
+++ inc/rf_modeler.h
@@ -0,0 +1,56 @@
+/* Represents the entire workspace with a single type of model. */
+#ifndef RF_MODELER_H
+#define RF_MODELER_H
+
+#include "rf_string.h"
+
+#include <stddef.h>
+
+/* A node in the workspace forest. Might be a source or module. Also it is a
+ * package if its at the top level. */
+struct rf_workspace_node {
+  /* Name of the source or module. */
+  struct rf_string name;
+
+  union {
+    /* Identifier of the source model. */
+    int model_id;
+    /* Number of sources and submodules that are directly under the module. */
+    int children_count;
+  };
+
+  /* Variant of the workspace node. */
+  enum rf_workspace_node_variant {
+    /* Node as source. */
+    RF_SOURCE,
+    /* Node as module. */
+    RF_MODULE,
+  }
+  /* Variant of the node. */
+  variant;
+};
+
+/* Forest of sources and modules, where the roots are packages. The children
+ * come before the parent. Modules are branches, while the sources are
+ * leaves. */
+struct rf_workspace {
+  /* Pointer to the first element of the array of nodes. */
+  struct rf_workspace_node* array;
+  /* Number of nodes stored in the array. */
+  size_t                    count;
+  /* Number of allocated node space in the array. */
+  size_t                    limit;
+};
+
+// =================================================
+//    }-{   P U B L I C   F U N C T I O N S   }-{
+// =================================================
+
+/* Adds the given node to the end of the given workspace. Returns whether the
+ * operation failed. Sets `errno` on failure. */
+bool rf_push_workspace_node(struct rf_workspace* target,
+  struct rf_workspace_node                       pushed);
+/* Frees the resources owned by the given workspace. */
+void rf_free_workspace(struct rf_workspace* freed);
+
+#endif // RF_MODELER_H
---
+++ src/rf_modeler.c
@@ -0,0 +1,55 @@
+#include "rf_modeler.h"
+
+#include "rf_allocator.h"
+
+#include <stdlib.h>
+
+/* Ensures that the given amount of space for nodes exists at the end of the
+ * given workspace forest. Does amortized growth if necessary. Returns whether
+ * the operation failed. Sets `errno` on failure. */
+static bool ensure_nodes(struct rf_workspace* target, size_t amount);
+
+// =================================================
+//    }-{   P U B L I C   F U N C T I O N S   }-{
+// =================================================
+
+bool rf_push_workspace_node(struct rf_workspace* target,
+  struct rf_workspace_node                       pushed) {
+  if (ensure_nodes(target, 1)) { return true; }
+  target->array[target->count++] = pushed;
+  return false;
+}
+
+void rf_free_workspace(struct rf_workspace* freed) {
+  RF_FREE(&freed->array);
+  freed->count = 0;
+  freed->limit = 0;
+}
+
+// ===================================================
+//    [+]   P R I V A T E   F U N C T I O N S   [+]
+// ===================================================
+
+static bool ensure_nodes(struct rf_workspace* target, size_t amount) {
+  // Check whether there is a need for growth.
+  size_t space = target->limit - target->count;
+  if (space >= amount) { return false; }
+
+  // If the list did not exist previously, allocate it.
+  if (target->array == NULL) {
+    if (RF_ALLOCATE_ARRAY(&target->array, amount, struct rf_workspace_node)) {
+      return true;
+    }
+    target->limit = amount;
+    return false;
+  }
+
+  // Grow at least by the half of the current limit to amortize allocations.
+  size_t new_limit = target->limit + max(amount, target->limit / 2);
+  if (RF_REALLOCATE_ARRAY(&target->array, new_limit,
+        struct rf_workspace_node)) {
+    return true;
+  }
+  target->limit = new_limit;
+  return false;
+}
