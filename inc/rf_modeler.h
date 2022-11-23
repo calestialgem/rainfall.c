d4e1fc3e785632147a5fa6dd0e59f263c31069bd
Cem GEÇGEL <gecgelcem@outlook.com>
2022-11-23 16:01:41 +0300

    refactor: Modeler

--- inc/rf_modeler.h
+++ inc/rf_modeler.h
@@ -11,40 +11,40 @@ enum {
   RF_SOURCE_MODELS_LIMIT = 1024,
 };
 
-/* A node in the workspace forest. Might be a source or module. Also it is a
- * package if its at the top level. */
-struct rf_workspace_node {
-  /* Name of the source or module. */
-  struct rf_string name;
-
-  union {
-    /* Identifier of the source model. */
-    int model_id;
-    /* Number of sources and submodules that are directly under the module. */
-    int children_count;
-  };
-
-  /* Variant of the workspace node. */
-  enum rf_workspace_node_variant {
-    /* Node as source. */
-    RF_SOURCE,
-    /* Node as module. */
-    RF_MODULE,
-  }
-  /* Variant of the node. */
-  variant;
-};
-
 /* Forest of sources and modules, where the roots are packages. The children
  * come before the parent. Modules are branches, while the sources are
  * leaves. */
 struct rf_workspace {
-  /* Pointer to the first element of the array of nodes. */
-  struct rf_workspace_node* array;
+  /* A node in the workspace forest. Might be a source or module. Also it is a
+   * package if its at the top level. */
+  struct rf_workspace_node {
+    /* Name of the source or module. */
+    struct rf_string name;
+
+    union {
+      /* Identifier of the source model. */
+      int model_id;
+      /* Number of sources and submodules that are directly under the module. */
+      int children_count;
+    };
+
+    /* Variant of the workspace node. */
+    enum rf_workspace_node_variant {
+      /* Node as source. */
+      RF_SOURCE,
+      /* Node as module. */
+      RF_MODULE,
+    }
+    /* Variant of the node. */
+    variant;
+  }
+    /* Pointer to the first element of the array of nodes. */
+    * array;
+
   /* Number of nodes stored in the array. */
-  size_t                    count;
+  size_t count;
   /* Number of allocated node space in the array. */
-  size_t                    limit;
+  size_t limit;
 };
 
 // =================================================
